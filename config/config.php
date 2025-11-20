afb85b62ed8c344a32136a8fa24ced667229a42d
Joona Puustelli <p43849@edu.sasky.fi>
2025-11-20 15:51:11 +0200

    tietokantakäsittelyn kääreluokka

--- config/config.php
+++ config/config.php
@@ -1,8 +1,14 @@
 <?php
 
   $config = array(
+    "db" => array(
+       "dbname" => $_SERVER["DB_DATABASE"],
+       "username" => $_SERVER["DB_USERNAME"],
+       "password" => $_SERVER["DB_PASSWORD"],
+       "host" => "localhost"
+    ),
     "urls" => array(
-      "baseUrl" => "/~p43849/lanify"
+        "baseUrl" => "/~p43849/lanify"
     )
   );
 
