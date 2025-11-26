d4dea422d3d319311e7ca068c1a2b84f073db568
Joona Puustelli <p43849@edu.sasky.fi>
2025-11-26 22:06:09 +0200

    istunnon aloitus

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
-
+  // Aloitetaan istunnot.
+  session_start();
 // Suoritetaan projektin alustusskripti.
 require_once '../src/init.php';
   // Siistitään polku urlin alusta ja mahdolliset parametrit urlin lopusta.
@@ -56,7 +57,8 @@ require_once '../src/init.php';
       if (isset($_POST['laheta'])) {
         require_once CONTROLLER_DIR . 'kirjaudu.php';
         if (tarkistaKirjautuminen($_POST['email'],$_POST['salasana'])) {
-          echo "Kirjautuminen ok!";
+           $_SESSION['user'] = $_POST['email'];
+           header("Location: " . $config['urls']['baseUrl']);
         } else {
           echo $templates->render('kirjaudu', [ 'error' => ['virhe' => 'Väärä käyttäjätunnus tai salasana!']]);
         }
